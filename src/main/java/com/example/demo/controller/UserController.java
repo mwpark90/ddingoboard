9ac02094572673d6caf3955fd0abb423e3d80b3a
mwpark <alsdn0528@gmail.com>
2021-03-20 23:41:25 +0900

    grafana connection

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -34,6 +37,7 @@ public class UserController {
     // 메인 페이지
     @GetMapping("/")
     public String index() {
+
         return "login";
     }
 
